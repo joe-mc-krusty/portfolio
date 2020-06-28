076155993dc72ac7b013d7af896276f3defe3b6b
JoeMcKrusty <joemckrusty@gmail.com>
2020-06-29 01:17:23 +0200

    about section component

---
+++ src/components/About/About.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+
+import "./About.scss"
+
+interface AboutProps {}
+interface AboutState {}
+
+class About extends React.Component<AboutProps, AboutState> {
+  render() {
+    return (
+      <section id="About" title="About" className="About">
+        <h2 className="title is-2">About Me</h2>
+        <h2 className="title is-2">About Me</h2>
+        <h2 className="title is-2">About Me</h2>
+        <h2 className="title is-2">About Me</h2>
+        <h2 className="title is-2">About Me</h2>
+        <h2 className="title is-2">About Me</h2>
+        <h2 className="title is-2">About Me</h2>
+        <h2 className="title is-2">About Me</h2>
+        <h2 className="title is-2">About Me</h2>
+        <h2 className="title is-2">About Me</h2>
+        <h2 className="title is-2">About Me</h2>
+        <h2 className="title is-2">About Me</h2>
+        <h2 className="title is-2">About Me</h2>
+        <h2 className="title is-2">About Me</h2>
+        <h2 className="title is-2">About Me</h2>
+      </section>
+    )
+  }
+}
+
+export default About
