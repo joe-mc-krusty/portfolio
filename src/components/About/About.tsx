bfb7e62e78c5c871f108af8bfee5b4505a8fa30a
JoeMcKrusty <joemckrusty@gmail.com>
2020-07-01 11:30:32 +0200

    About component update

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -14,18 +14,53 @@ class About extends React.Component<AboutProps, AboutState> {
     return (
       <section id="About" title="About" className="About">
         <div className="columns">
-          <div className="column is-2 is-offset-3">
+          <div className="column is-6 is-offset-3">
+          <h2 className="title is-2">About me</h2>
             <Image
               className="about-pic"
               filename="about-pic.png"
               alt="About picture"
-              imgStyle={{
-                height: "450px",
-              }}
+              imgStyle={{}}
             />
-          </div>
-          <div className="column is-4">
-            <h2 className="subtitle is-5">About me</h2>
+            
+            <p className="content">
+              I started my dev adventure as a <strong>PHP</strong> developer and
+              have worked on quite a few frameworks and CMSs, notably{" "}
+              <strong>Symfony</strong>,<strong>Wordpress</strong> &{" "}
+              <strong>Magento</strong>.
+            </p>
+            <p className="content">
+              <strong>Javascript</strong> has always been part of my tech stack
+              and took over time a larger place, to the point where it became
+              the main language I now use on my job and personal projects. I've
+              been working on both frontend and backend with the following
+              frameworks: <strong>React</strong>, <strong>Angular</strong>,{" "}
+              <strong>NestJS (node)</strong>
+            </p>
+            <p className="content">
+              I like to automate everyhting I touch & being a Unix systems
+              enthusiast, I naturally made my way into system administration and
+              automation. <strong>Docker</strong> & <strong>Ansible</strong> are
+              now my daily companions. When developing CLI tools and various
+              system scripts, <strong>Python</strong> is my partner of choice.
+            </p>
+            <p className="content">
+              Based in France but have no problem with working 100% remote, as
+              I'm familiar with most commonly used collaboration tools:{" "}
+              <strong>Slack</strong>, <strong>Microsoft Teams</strong>,{" "}
+              <strong>CodeStream</strong>, <strong>Github</strong>,{" "}
+              <strong>Gitlab</strong>,{" "}
+              <strong>Atlassian Suite (Jira, Confluence)</strong>,{" "}
+              <strong>Clockify</strong>, <strong>Harvest</strong>. To name a
+              few.
+            </p>
+            <p className="content">
+              Formerly expatriated in the Philippines for several years, I'm now
+              living back in my home country and a proud dad of 3. During free
+              time I like spending time with my kids, reading and learning about
+              new techs & learning foreign languages.
+            </p>
+            <p>Arch linux is my daily driver. I use Arch btw.</p>
           </div>
         </div>
       </section>
