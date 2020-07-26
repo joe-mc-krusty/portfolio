4fcfd3aa4e24ee061130edbfdf3b993d7c965d9e
JoeMcKrusty <joemckrusty@gmail.com>
2020-07-26 03:26:27 +0200

    new libs
    - iconify
    - gatsby-plugin-react-svg
    - react slick
    - slick-carousel (css)

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,14 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /svg\/.*\.svg/,
+        },
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
