7997c59036ed2646c7403cd608e53babd0e6a7df
JoeMcKrusty <joemckrusty@gmail.com>
2020-08-12 22:24:22 +0200

    fix form name for netlify

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -31,7 +31,7 @@ const Contact = () => {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "xs-contact", ...data }),
+      body: encode({ "form-name": "contact", ...data }),
     })
       .then(() => alert("Success!"))
       .catch(error => alert(error))
